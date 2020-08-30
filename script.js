13348262030a832b0840dca57e7443cc01f45ccd
Dina Saunders <dina_saunders@mac.com>
2020-08-30 16:56:20 -0400

    added initial start, question and answer functions

---
+++ script.js
@@ -0,0 +1,24 @@
+var startButton = document.getElementById("start-btn")
+var questionContainer = document.getElementById("question-container")
+
+startButton.addEventListener("click", startGame)
+
+function startGame() {
+
+    startButton.classList.add("hide")
+    questionContainer.classList.remove("hide")
+    setNextQuestion()
+    
+    console.log("Started")
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer () {
+
+}
+
+// Need to set up Question and Answer Indexes
+var questions 
