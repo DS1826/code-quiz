e13c1da084a3df2d5a79600d8623b75209254689
Dina Saunders <dina_saunders@mac.com>
2020-09-04 11:20:35 -0400

    revised start btn element and added question content

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-var startButton = document.getElementById("start-btn")
+var startButton = document.getElementById("play-btn")
 var questionContainer = document.getElementById("question-container")
+
+var shuffledQuestions;
+var currentQuestionIndex;
 
 startButton.addEventListener("click", startGame)
 
+// Start the game by displaying the first question with answer selections and next button
 function startGame() {
-
-    startButton.classList.add("hide")
+    console.log("Started")
+    shuffleQuestions = questions.sort();
     questionContainer.classList.remove("hide")
     setNextQuestion()
     
-    console.log("Started")
+    
 }
 
 function setNextQuestion() {
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
+function showQuestion(question) {
+    
 }
 
 function selectAnswer () {
@@ -21,4 +29,14 @@ function selectAnswer () {
 }
 
 // Need to set up Question and Answer Indexes
-var questions 
+var questions = [
+    {
+        question: "Which answer does NOT describe Javascript?",
+        answers: [
+            {text: "Enables dynamically updated content", correct: false},
+            {text: "It is a scripting language", correct: false},
+            {text: "It is a layer of standard web technology", correct: false},
+            {text: "It is a markup language that structures web content", correct: true}
+        ]
+    }
+]
