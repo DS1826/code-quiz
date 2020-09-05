a26fe4274ce9a672783d0e19423a4261916fdf36
Dina Saunders <dina_saunders@mac.com>
2020-09-05 18:03:44 -0400

    added incremental score keeping statement to selected answer function

--- script.js
+++ script.js
@@ -3,15 +3,14 @@ var question = document.getElementById("question");
 var quizContent = document.getElementById("questions-container");
 var answer = document.getElementsByClassName("answer-btn");
 var scoresButton = document.getElementById("scores-btn");
-var newChoice1 = document.getElementById("answer-1");
+let newChoice1 = document.getElementById("answer-1");
 let newChoice2 = document.getElementById("answer-2");
 let newChoice3 = document.getElementById("answer-3");
 let newChoice4 = document.getElementById("answer-4");
+var recentScore = document.getElementById("score");
 
-var currentQuestion;
 var score = 0;
 var questionCounter = 0;
-var availableQuestions = 0;
 
 startButton.addEventListener("click", startGame);
 quizContent.classList.add("hidden");
@@ -22,6 +21,7 @@ function startGame() {
     startButton.classList.add("hidden");
     scoresButton.classList.add("hidden");
     quizContent.classList.remove("hidden");
+    recentScore.textContent = ("Current Score = " + score);
     getNextQuestion();
 }
 
@@ -32,7 +32,7 @@ function getNextQuestion() {
 
     newChoice1.textContent = questions[questionCounter].choice1;
     newChoice1.addEventListener("click", selectAnswer);
-    
+
     newChoice2.textContent = questions[questionCounter].choice2;
     newChoice2.addEventListener("click", selectAnswer);
 
@@ -51,31 +51,36 @@ function selectAnswer() {
     console.log(answerSelected);
     if (answerSelected === questions[questionCounter].answer) {
         console.log("correct");
+        document.getElementById("score").innerHTML = ("Current Score = " + (score+=10));
     } else {
         console.log("incorrect");
     }
 
     questionCounter++;
-    
 
-    if (questionCounter >= 0 && questionCounter < 3) {
+    if (questionCounter >= 0 && questionCounter < 4) {
         getNextQuestion();
     } else {
-        document.getElementById("questions-container").innerHTML = "";
-        document.getElementById("main-title").textContent = "Game Over";
-        scoresButton.classList.remove("hidden");
-        scoresButton.addEventListener("click", highScores)
+        gameOver();
     }
 }
 
-// Displays local storage record of user high scores.
-function highScores() {
-    document.getElementById("main-title").textContent = "High Scores";
-    scoresButton.classList.add("hidden");
+function gameOver() {
+    document.getElementById("questions-container").innerHTML = "";
+    document.getElementById("main-title").textContent = "Game Over";
 
 }
 
+// Displays High Scores from home page button
+scoresButton.addEventListener("click", highScores) 
 
+function highScores() {
+    gameOver();
+    startButton.classList.add("hidden");
+    scoresButton.classList.add("hidden");
+    document.getElementById("main-title").textContent = "High Scores";
+    
+}
 
 // Question and Answer Index
 var questions = [
@@ -83,28 +88,36 @@ var questions = [
         question: "Which answer does NOT describe Javascript?",
         choice1: "A. Enables dynamically updated content",
         choice2: "B. It is a markup language that structures web content",
-        choice3: "C. It is a layer of standard web technology", 
+        choice3: "C. It is a layer of standard web technology",
         choice4: "D. It is a scripting language",
         answer: "B. It is a markup language that structures web content"
     }
-,
+    ,
     {
         question: "What was JavaScript originally named?",
         choice1: "A. Mocha",
         choice2: "B. Pache",
-        choice3: "C. Venecia", 
+        choice3: "C. Venecia",
         choice4: "D. Casiopea",
         answer: "A. Mocha"
     }
-,
+    ,
     {
         question: "Which statements about variables are TRUE?",
         choice1: "A. They hold data values",
         choice2: "B. A variable can be changed any time",
-        choice3: "C. Values are assigned to a variable using the equal to operator", 
+        choice3: "C. Values are assigned to a variable using the equal to operator",
         choice4: "D. All of the above",
         answer: "D. All of the above"
     }
+    ,
+    {
+        question: "JavaScript is put inside which HTML element?",
+        choice1: "A. <java>",
+        choice2: "B. <js>",
+        choice3: "C. <script>",
+        choice4: "D. <javascript>",
+        answer: "C. <script>"
+    }
 ];
-
 
